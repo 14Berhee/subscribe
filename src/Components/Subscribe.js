c98e7044b85fdc6ca69ececa57fef2696f3b05b3
24LP4300 24LP4300 <24LP4300@G305-CC0063.local>
2024-08-14 09:53:05 +0800

    subscribe header hiisen

---
+++ src/Components/Subscribe.js
@@ -0,0 +1,13 @@
+import styles from "../styles/Subscribe.module.css";
+
+const Subscribe = () => {
+  return (
+    <div className={styles.Subscribe}>
+      <div>
+        <h1>Subscribe</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Subscribe;
